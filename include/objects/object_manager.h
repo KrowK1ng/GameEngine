f7b6e79ce2f6e935d53ff5e52eb83513546ff56f
KrowK1ng <victor.vorona2004@gmail.com>
2021-03-11 19:39:28 +0200

    deleted junk

--- include/objects/object_manager.h
+++
@@ -1,7 +0,0 @@
-#include "gengine.h"
-#include "objects/obj_floor.h"
-#include "objects/obj_player.h"
-
-namespace object{
-	Object* CreateObject(std::string name, int x, int y);
-}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-//#include "../engine-source/engine.h"
-#include "engine.h"
-
-int main(){
-	std::cout << "start main" << std::endl;
-	engine::StartGame( "Chess Super Cool", 60, 512, 512, false);
-	std::cout << "ended main";
-}
